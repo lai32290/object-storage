e53602e6d5739fb493da6e102e770cfb5c33625f
Lai Xuancheng <lai32690@gmail.com>
2019-12-21 11:27:50 -0300

    add auth route

--- index.js
+++ index.js
@@ -8,13 +8,21 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send(data);
+    res.send({ data, auth: process.env.OBJECT_STORAGE_AUTH });
 });
 app.post('/', (req, res) => {
     data = req.body.data;
     res.send(data);
 });
 
+app.post('/auth', (req, res) => {
+    const auth = req.body.auth;
+    process.env.OBJECT_STORAGE_AUTH = auth;
+
+    res.send();
+});
+
 app.listen(port, () => {
     console.log(`server is listen on http://localhost:${port}`);
 });
+
